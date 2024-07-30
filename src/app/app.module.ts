f821a0aa531913e0e6df9f9d9b0111527fbccf6a
Tamás B. Nemes <tb.nemes@gmail.com>
2024-07-30 22:09:37 +0200

    generated pages and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
